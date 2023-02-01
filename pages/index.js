df42374f95fa0a6024f83fb2a162d052ef14d0f8
Benjamin Sloutsky <64224906+bslou@users.noreply.github.com>
2023-02-01 13:32:29 -0800

    update it

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,88 @@ export default function Home() {
   const router = useRouter();
   const toast = useToast();
   return (
-    <Flex
-      direction={"column"}
-      alignItems={"center"}
-      backgroundColor={"#efefef"}
-      width={"100vw"}
-      gap={8}
-    >
+    <>
+      <Head>
+        <title>
+          PicAnswer - Answer any question you have by just taking a picture
+        </title>
+        <meta
+          name="description"
+          content="PicAnswer integrated AI within picture taking, so now you can take
+          pictures and use ChatGPT in order to solve any problem you have."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Flex
-        direction={"row"}
+        direction={"column"}
         alignItems={"center"}
-        justifyContent={"space-between"}
-        width={"100%"}
-        paddingLeft={5}
-        paddingTop={0}
+        backgroundColor={"#efefef"}
+        width={"100vw"}
+        gap={8}
       >
-        <Button
-          colorScheme={"transparent"}
-          fontFamily={"sans-serif"}
-          color={"black"}
-          onClick={() => router.push("/")}
+        <Flex
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          paddingLeft={5}
+          paddingTop={0}
         >
-          📷 PicAnswer
-        </Button>
+          <Button
+            colorScheme={"transparent"}
+            fontFamily={"sans-serif"}
+            color={"black"}
+            onClick={() => router.push("/")}
+          >
+            📷 PicAnswer
+          </Button>
+          <Button
+            onClick={() =>
+              toast({
+                title: "App is not released yet",
+                status: "info",
+                duration: 9000,
+                isClosable: true,
+              })
+            }
+            colorScheme={"transparent"}
+            width={250}
+            height={100}
+          >
+            <Image
+              src={"/assets/appstore.png"}
+              alt={"App Store Download - PicAnswer"}
+              layout={"fill"}
+            />
+          </Button>
+        </Flex>
+        <Flex
+          direction={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          width={"55%"}
+          gap={2}
+        >
+          <Text
+            fontWeight={500}
+            fontSize={"40pt"}
+            textAlign={"center"}
+            fontFamily={"sans-serif"}
+          >
+            Answer any question you have with a click of a button.
+          </Text>
+          <Text
+            color={"gray"}
+            fontSize={"21pt"}
+            textAlign={"center"}
+            fontFamily={"sans-serif"}
+            fontWeight={300}
+          >
+            PicAnswer integrated AI within picture taking, so now you can take
+            pictures and use ChatGPT in order to solve any problem you have.
+          </Text>
+        </Flex>
         <Button
           onClick={() =>
             toast({
@@ -41,8 +100,8 @@ export default function Home() {
             })
           }
           colorScheme={"transparent"}
-          width={250}
-          height={100}
+          width={320}
+          height={130}
         >
           <Image
             src={"/assets/appstore.png"}
@@ -50,95 +109,50 @@ export default function Home() {
             layout={"fill"}
           />
         </Button>
-      </Flex>
-      <Flex
-        direction={"column"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        width={"55%"}
-        gap={2}
-      >
-        <Text
-          fontWeight={500}
-          fontSize={"40pt"}
-          textAlign={"center"}
-          fontFamily={"sans-serif"}
-        >
-          Answer any question you have with a click of a button.
-        </Text>
-        <Text
-          color={"gray"}
-          fontSize={"21pt"}
-          textAlign={"center"}
-          fontFamily={"sans-serif"}
-          fontWeight={300}
-        >
-          PicAnswer integrated AI within picture taking, so now you can take
-          pictures and use ChatGPT in order to solve any problem you have.
-        </Text>
-      </Flex>
-      <Button
-        onClick={() =>
-          toast({
-            title: "App is not released yet",
-            status: "info",
-            duration: 9000,
-            isClosable: true,
-          })
-        }
-        colorScheme={"transparent"}
-        width={320}
-        height={130}
-      >
-        <Image
-          src={"/assets/appstore.png"}
-          alt={"App Store Download - PicAnswer"}
-          layout={"fill"}
+        <video
+          controls
+          src={"/assets/RPReplay.mp4"}
+          style={{ width: "80vw", height: "400px" }}
         />
-      </Button>
-      <video
-        controls
-        src={"/assets/RPReplay.mp4"}
-        style={{ width: "80vw", height: "400px" }}
-      />
-      <br />
-      <Flex
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        width={"100vw"}
-        borderTop={"1px solid #cdcdcd"}
-        paddingTop={5}
-        paddingBottom={5}
-        gap={10}
-      >
-        <Link
-          color={"gray"}
-          onClick={() =>
-            router.push(
-              "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRQKmTFrqpgZTsSwWvzQgWcmNkZkFdspqcQdZstvkMNJSRprkVTgvcVRKqxjsCnkTbQmpq"
-            )
-          }
-          passHref
-          target={"_blank"}
+        <br />
+        <Flex
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          width={"100vw"}
+          borderTop={"1px solid #cdcdcd"}
+          paddingTop={5}
+          paddingBottom={5}
+          gap={10}
         >
-          Contact
-        </Link>
-        <Link
-          color={"gray"}
-          onClick={() => router.push("/privacy")}
-          target={"_blank"}
-        >
-          Privacy
-        </Link>
-        <Link
-          color={"gray"}
-          onClick={() => router.push("/terms")}
-          target={"_blank"}
-        >
-          Terms
-        </Link>
+          <Link
+            color={"gray"}
+            onClick={() =>
+              router.push(
+                "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRQKmTFrqpgZTsSwWvzQgWcmNkZkFdspqcQdZstvkMNJSRprkVTgvcVRKqxjsCnkTbQmpq"
+              )
+            }
+            passHref
+            target={"_blank"}
+          >
+            Contact
+          </Link>
+          <Link
+            color={"gray"}
+            onClick={() => router.push("/privacy")}
+            target={"_blank"}
+          >
+            Privacy
+          </Link>
+          <Link
+            color={"gray"}
+            onClick={() => router.push("/terms")}
+            target={"_blank"}
+          >
+            Terms
+          </Link>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
--- pages/terms.js
+++ pages/terms.js
@@ -8,146 +8,158 @@ export default function Terms() {
   const router = useRouter();
   const toast = useToast();
   return (
-    <Flex
-      direction={"column"}
-      alignItems={"center"}
-      backgroundColor={"#efefef"}
-      width={"100vw"}
-      gap={8}
-    >
-      <Flex
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        width={"100%"}
-        paddingLeft={5}
-        paddingTop={0}
-      >
-        <Button
-          colorScheme={"transparent"}
-          fontFamily={"sans-serif"}
-          color={"black"}
-          onClick={() => router.push("/")}
-        >
-          📷 PicAnswer
-        </Button>
-        <Button
-          onClick={() =>
-            toast({
-              title: "App is not released yet",
-              status: "info",
-              duration: 9000,
-              isClosable: true,
-            })
-          }
-          colorScheme={"transparent"}
-          width={250}
-          height={100}
-        >
-          <Image
-            src={"/assets/appstore.png"}
-            alt={"App Store Download - PicAnswer"}
-            layout={"fill"}
-          />
-        </Button>
-      </Flex>
+    <>
+      <Head>
+        <title>PicAnswer - Terms & Conditions</title>
+        <meta
+          name="description"
+          content="PicAnswer integrated AI within picture taking, so now you can take
+          pictures and use ChatGPT in order to solve any problem you have."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Flex
         direction={"column"}
         alignItems={"center"}
-        justifyContent={"center"}
-        width={"50%"}
-      >
-        <Text fontSize={"35pt"}>Terms & Conditions</Text>
-        <br />
-        <Text>
-          Introduction: PicAnswer is a photo-taking app that integrates
-          artificial intelligence to provide users with solutions to their
-          problems. By using PicAnswer, you agree to be bound by these terms and
-          conditions. <br />
-          <br />
-          1.&nbsp;&nbsp;User Account: To use the app, you must create an
-          account. You are responsible for maintaining the confidentiality of
-          your account information and for any activities that occur under your
-          account. <br />
-          <br />
-          2.&nbsp;&nbsp;User Content: When you use PicAnswer, you may provide
-          pictures or other content. You retain ownership of any intellectual
-          property rights that you hold in your content, but you grant PicAnswer
-          a non-exclusive, royalty-free, transferable, sub-licensable, worldwide
-          license to use, copy, modify, and distribute your content in
-          connection with the PicAnswer services.
-          <br />
-          <br />
-          3.&nbsp;&nbsp;Limitation of Liability: PicAnswer is not responsible
-          for any damages or losses resulting from the use of the app, including
-          without limitation, indirect, incidental, special, punitive, or
-          consequential damages. <br />
-          <br />
-          4.&nbsp;&nbsp;Disclaimer of Warranties: PicAnswer provides the app on
-          an "as is" and "as available" basis. PicAnswer makes no
-          representations or warranties of any kind, express or implied, as to
-          the operation of the app or the information, content, materials, or
-          products included in the app.
-          <br />
-          <br />
-          5.&nbsp;&nbsp;Indemnification: You agree to indemnify and hold
-          PicAnswer harmless from any claims, losses, liabilities, damages, or
-          expenses arising from your use of the app or from your breach of these
-          terms and conditions. <br />
-          <br />
-          6.&nbsp;&nbsp;Changes to Terms: PicAnswer reserves the right to modify
-          these terms and conditions at any time without prior notice. Your
-          continued use of the app after any changes indicates your acceptance
-          of the new terms. <br />
-          <br />
-          7.&nbsp;&nbsp;Governing Law: These terms and conditions are governed
-          by the laws of the jurisdiction in which PicAnswer is headquartered
-          and any applicable federal laws of the United States.
-          <br />
-          <br />
-          By using the PicAnswer app, you agree to be bound by these terms and
-          conditions. If you do not agree to these terms and conditions, you
-          should not use the app.
-        </Text>
-      </Flex>
-      <br />
-      <Flex
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"center"}
+        backgroundColor={"#efefef"}
         width={"100vw"}
-        borderTop={"1px solid #cdcdcd"}
-        paddingTop={5}
-        paddingBottom={5}
-        gap={10}
+        gap={8}
       >
-        <Link
-          color={"gray"}
-          onClick={() =>
-            router.push(
-              "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRQKmTFrqpgZTsSwWvzQgWcmNkZkFdspqcQdZstvkMNJSRprkVTgvcVRKqxjsCnkTbQmpq"
-            )
-          }
-          passHref
-          target={"_blank"}
+        <Flex
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          paddingLeft={5}
+          paddingTop={0}
         >
-          Contact
-        </Link>
-        <Link
-          color={"gray"}
-          onClick={() => router.push("/privacy")}
-          target={"_blank"}
+          <Button
+            colorScheme={"transparent"}
+            fontFamily={"sans-serif"}
+            color={"black"}
+            onClick={() => router.push("/")}
+          >
+            📷 PicAnswer
+          </Button>
+          <Button
+            onClick={() =>
+              toast({
+                title: "App is not released yet",
+                status: "info",
+                duration: 9000,
+                isClosable: true,
+              })
+            }
+            colorScheme={"transparent"}
+            width={250}
+            height={100}
+          >
+            <Image
+              src={"/assets/appstore.png"}
+              alt={"App Store Download - PicAnswer"}
+              layout={"fill"}
+            />
+          </Button>
+        </Flex>
+        <Flex
+          direction={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          width={"50%"}
         >
-          Privacy
-        </Link>
-        <Link
-          color={"gray"}
-          onClick={() => router.push("/terms")}
-          target={"_blank"}
+          <Text fontSize={"35pt"}>Terms & Conditions</Text>
+          <br />
+          <Text>
+            Introduction: PicAnswer is a photo-taking app that integrates
+            artificial intelligence to provide users with solutions to their
+            problems. By using PicAnswer, you agree to be bound by these terms
+            and conditions. <br />
+            <br />
+            1.&nbsp;&nbsp;User Account: To use the app, you must create an
+            account. You are responsible for maintaining the confidentiality of
+            your account information and for any activities that occur under
+            your account. <br />
+            <br />
+            2.&nbsp;&nbsp;User Content: When you use PicAnswer, you may provide
+            pictures or other content. You retain ownership of any intellectual
+            property rights that you hold in your content, but you grant
+            PicAnswer a non-exclusive, royalty-free, transferable,
+            sub-licensable, worldwide license to use, copy, modify, and
+            distribute your content in connection with the PicAnswer services.
+            <br />
+            <br />
+            3.&nbsp;&nbsp;Limitation of Liability: PicAnswer is not responsible
+            for any damages or losses resulting from the use of the app,
+            including without limitation, indirect, incidental, special,
+            punitive, or consequential damages. <br />
+            <br />
+            4.&nbsp;&nbsp;Disclaimer of Warranties: PicAnswer provides the app
+            on an "as is" and "as available" basis. PicAnswer makes no
+            representations or warranties of any kind, express or implied, as to
+            the operation of the app or the information, content, materials, or
+            products included in the app.
+            <br />
+            <br />
+            5.&nbsp;&nbsp;Indemnification: You agree to indemnify and hold
+            PicAnswer harmless from any claims, losses, liabilities, damages, or
+            expenses arising from your use of the app or from your breach of
+            these terms and conditions. <br />
+            <br />
+            6.&nbsp;&nbsp;Changes to Terms: PicAnswer reserves the right to
+            modify these terms and conditions at any time without prior notice.
+            Your continued use of the app after any changes indicates your
+            acceptance of the new terms. <br />
+            <br />
+            7.&nbsp;&nbsp;Governing Law: These terms and conditions are governed
+            by the laws of the jurisdiction in which PicAnswer is headquartered
+            and any applicable federal laws of the United States.
+            <br />
+            <br />
+            By using the PicAnswer app, you agree to be bound by these terms and
+            conditions. If you do not agree to these terms and conditions, you
+            should not use the app.
+          </Text>
+        </Flex>
+        <br />
+        <Flex
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          width={"100vw"}
+          borderTop={"1px solid #cdcdcd"}
+          paddingTop={5}
+          paddingBottom={5}
+          gap={10}
         >
-          Terms
-        </Link>
+          <Link
+            color={"gray"}
+            onClick={() =>
+              router.push(
+                "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRQKmTFrqpgZTsSwWvzQgWcmNkZkFdspqcQdZstvkMNJSRprkVTgvcVRKqxjsCnkTbQmpq"
+              )
+            }
+            passHref
+            target={"_blank"}
+          >
+            Contact
+          </Link>
+          <Link
+            color={"gray"}
+            onClick={() => router.push("/privacy")}
+            target={"_blank"}
+          >
+            Privacy
+          </Link>
+          <Link
+            color={"gray"}
+            onClick={() => router.push("/terms")}
+            target={"_blank"}
+          >
+            Terms
+          </Link>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
